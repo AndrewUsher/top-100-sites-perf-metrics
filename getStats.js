6498fe1731d7be03bb67741f0e56da2233c86c15
Andrew Usher <andrewusher00@gmail.com>
2019-01-16 13:02:56 -0600

    feat(Get Stats): Add functionality to parse Lighthouse reports and generate lines that VictoryCharts

--- getStats.js
+++ getStats.js
@@ -1,11 +1,60 @@
 const { exec } = require('child_process')
+const { readdir, readFile, writeFile } = require('fs')
 const { promisify } = require('util')
 const execAsync = promisify(exec)
+const readdirAsync = promisify(readdir)
+const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
+const path = require('path')
 let sites = require('./sites')
 
-sites = sites.slice(0, 30).map(site => `https://${site}`)
+const stats = []
 ;(async () => {
-  console.log(sites)
-  console.log(sites.join(','))
-  await execAsync(`lighthouse-batch -s ${sites.join(',')}`)
+  const writeStatsToFile = async stats => {
+    await writeFileAsync('stats.js', `export default ${JSON.stringify(stats)}`)
+  }
+  const parseSites = async () => {
+    const pathToReports = path.resolve(process.cwd(), 'report/lighthouse')
+    const files = await readdirAsync(pathToReports)
+    const lines = await Promise.all(
+      files.map(async (file, index) => {
+        if (index === 2) {
+          return []
+        }
+        const data = await readFileAsync(path.resolve(pathToReports, file))
+        const lighthouseJSON = JSON.parse(data.toString())
+        const { audits } = lighthouseJSON
+        // console.log(audits)
+
+        const {
+          interactive: { displayValue: TTI },
+          'first-cpu-idle': { displayValue: FCI },
+          'first-meaningful-paint': { displayValue: FMP },
+          'first-contentful-paint': { displayValue: FCP }
+        } = audits
+        const line = [
+          {
+            x: 'First Contentful Paint',
+            y: Number(Number(FCP.slice(0, FCP.length - 2)).toFixed(2))
+          },
+          {
+            x: 'First Meaningful Paint',
+            y: Number(Number(FMP.slice(0, FMP.length - 2)).toFixed(2))
+          },
+          {
+            x: 'First CPU Idle',
+            y: Number(Number(FCI.slice(0, FCI.length - 2)).toFixed(2))
+          },
+          {
+            x: 'Time To Interactive',
+            y: Number(Number(TTI.slice(0, TTI.length - 2)).toFixed(2))
+          }
+        ]
+        return line
+      })
+    )
+    await writeStatsToFile(lines)
+  }
+  await execAsync(`node batcher.js -s ${sites.join(',')}`)
+  await parseSites()
 })()
