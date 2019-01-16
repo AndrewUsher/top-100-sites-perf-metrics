99db62e8a66af84934de4429e0320751dd19092d
Andrew Usher <andrewusher00@gmail.com>
2019-01-16 13:00:41 -0600

    feat(Stats): Add generated stats file

---
+++ stats.js
@@ -0,0 +1,69 @@
+export default [
+  [
+    { x: 'First Contentful Paint', y: 10.5 },
+    { x: 'First Meaningful Paint', y: 11.4 },
+    { x: 'First CPU Idle', y: 11.4 },
+    { x: 'Time To Interactive', y: 11.5 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 5.2 },
+    { x: 'First Meaningful Paint', y: 5.6 },
+    { x: 'First CPU Idle', y: 5.6 },
+    { x: 'Time To Interactive', y: 5.8 }
+  ],
+  [],
+  [
+    { x: 'First Contentful Paint', y: 1.7 },
+    { x: 'First Meaningful Paint', y: 1.7 },
+    { x: 'First CPU Idle', y: 3.9 },
+    { x: 'Time To Interactive', y: 4.3 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 1.8 },
+    { x: 'First Meaningful Paint', y: 1.8 },
+    { x: 'First CPU Idle', y: 1.8 },
+    { x: 'Time To Interactive', y: 1.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.9 },
+    { x: 'First Meaningful Paint', y: 2.9 },
+    { x: 'First CPU Idle', y: 2.9 },
+    { x: 'Time To Interactive', y: 3.1 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.9 },
+    { x: 'First Meaningful Paint', y: 5.5 },
+    { x: 'First CPU Idle', y: 8.4 },
+    { x: 'Time To Interactive', y: 8.9 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.5 },
+    { x: 'First Meaningful Paint', y: 5.9 },
+    { x: 'First CPU Idle', y: 6 },
+    { x: 'Time To Interactive', y: 6.3 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 1.8 },
+    { x: 'First Meaningful Paint', y: 1.8 },
+    { x: 'First CPU Idle', y: 1.8 },
+    { x: 'Time To Interactive', y: 2 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.4 },
+    { x: 'First Meaningful Paint', y: 3.4 },
+    { x: 'First CPU Idle', y: 7.7 },
+    { x: 'Time To Interactive', y: 9.2 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.3 },
+    { x: 'First Meaningful Paint', y: 2.4 },
+    { x: 'First CPU Idle', y: 2.4 },
+    { x: 'Time To Interactive', y: 3.4 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.9 },
+    { x: 'First Meaningful Paint', y: 4.2 },
+    { x: 'First CPU Idle', y: 5.1 },
+    { x: 'Time To Interactive', y: 6.5 }
+  ]
+]
