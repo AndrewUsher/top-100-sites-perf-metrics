dcff5d483039891ed0ecf111dafe5b36115925cf
Andrew Usher <andrewusher00@gmail.com>
2019-01-17 08:10:22 -0600

    feat(Get Stats): Use spawn instead of exec to solve buffer issue

--- getStats.js
+++ getStats.js
@@ -1,7 +1,6 @@
-const { exec } = require('child_process')
+const { spawnSync } = require('child_process')
 const { readdir, readFile, writeFile } = require('fs')
 const { promisify } = require('util')
-const execAsync = promisify(exec)
 const readdirAsync = promisify(readdir)
 const readFileAsync = promisify(readFile)
 const writeFileAsync = promisify(writeFile)
@@ -32,29 +31,34 @@ const stats = []
           'first-meaningful-paint': { displayValue: FMP },
           'first-contentful-paint': { displayValue: FCP }
         } = audits
-        const line = [
-          {
-            x: 'First Contentful Paint',
-            y: Number(Number(FCP.slice(0, FCP.length - 2)).toFixed(2))
-          },
-          {
-            x: 'First Meaningful Paint',
-            y: Number(Number(FMP.slice(0, FMP.length - 2)).toFixed(2))
-          },
-          {
-            x: 'First CPU Idle',
-            y: Number(Number(FCI.slice(0, FCI.length - 2)).toFixed(2))
-          },
-          {
-            x: 'Time To Interactive',
-            y: Number(Number(TTI.slice(0, TTI.length - 2)).toFixed(2))
-          }
-        ]
-        return line
+        if (FCP && FMP && FCI && TTI) {
+          const line = [
+            {
+              x: 'First Contentful Paint',
+              y: Number(Number(FCP.slice(0, FCP.length - 2)).toFixed(2))
+            },
+            {
+              x: 'First Meaningful Paint',
+              y: Number(Number(FMP.slice(0, FMP.length - 2)).toFixed(2))
+            },
+            {
+              x: 'First CPU Idle',
+              y: Number(Number(FCI.slice(0, FCI.length - 2)).toFixed(2))
+            },
+            {
+              x: 'Time To Interactive',
+              y: Number(Number(TTI.slice(0, TTI.length - 2)).toFixed(2))
+            }
+          ]
+          return line
+        } else {
+          return []
+        }
       })
     )
     await writeStatsToFile(lines)
   }
-  await execAsync(`node batcher.js -s ${sites.join(',')}`)
+  // const cmd = spawnSync('node', ['batcher.js', '-s', sites.join(','), '-p', '-config-path=lighthouse.config.js'])
+  // cmd.stdout.on('data', d => console.log(d))
   await parseSites()
 })()
