1347af2b4d3335d141b107600de7672dc5019d60
Andrew Usher <andrewusher00@gmail.com>
2019-01-17 08:12:52 -0600

    feat(Stats Data): Commit stats for now (proxy issue)

--- stats.js
+++ stats.js
@@ -1,69 +1,550 @@
 export default [
   [
-    { x: 'First Contentful Paint', y: 10.5 },
-    { x: 'First Meaningful Paint', y: 11.4 },
-    { x: 'First CPU Idle', y: 11.4 },
-    { x: 'Time To Interactive', y: 11.5 }
+    { x: 'First Contentful Paint', y: 6.9 },
+    { x: 'First Meaningful Paint', y: 6.9 },
+    { x: 'First CPU Idle', y: 6.9 },
+    { x: 'Time To Interactive', y: 14.9 }
   ],
   [
-    { x: 'First Contentful Paint', y: 5.2 },
-    { x: 'First Meaningful Paint', y: 5.6 },
-    { x: 'First CPU Idle', y: 5.6 },
-    { x: 'Time To Interactive', y: 5.8 }
+    { x: 'First Contentful Paint', y: 4.2 },
+    { x: 'First Meaningful Paint', y: 4.2 },
+    { x: 'First CPU Idle', y: 9.1 },
+    { x: 'Time To Interactive', y: 16.3 }
   ],
   [],
   [
+    { x: 'First Contentful Paint', y: 3.6 },
+    { x: 'First Meaningful Paint', y: 4 },
+    { x: 'First CPU Idle', y: 12.5 },
+    { x: 'Time To Interactive', y: 13.5 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.5 },
+    { x: 'First Meaningful Paint', y: 4.5 },
+    { x: 'First CPU Idle', y: 10.4 },
+    { x: 'Time To Interactive', y: 12.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.2 },
+    { x: 'First Meaningful Paint', y: 4.4 },
+    { x: 'First CPU Idle', y: 13.1 },
+    { x: 'Time To Interactive', y: 19.4 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.6 },
+    { x: 'First Meaningful Paint', y: 4.6 },
+    { x: 'First CPU Idle', y: 9.9 },
+    { x: 'Time To Interactive', y: 14.1 }
+  ],
+  [],
+  [
+    { x: 'First Contentful Paint', y: 1.8 },
+    { x: 'First Meaningful Paint', y: 1.9 },
+    { x: 'First CPU Idle', y: 3.2 },
+    { x: 'Time To Interactive', y: 3.9 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.6 },
+    { x: 'First Meaningful Paint', y: 4.4 },
+    { x: 'First CPU Idle', y: 11.6 },
+    { x: 'Time To Interactive', y: 17.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 7.3 },
+    { x: 'First Meaningful Paint', y: 8.8 },
+    { x: 'First CPU Idle', y: 8.8 },
+    { x: 'Time To Interactive', y: 8.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.3 },
+    { x: 'First Meaningful Paint', y: 3.5 },
+    { x: 'First CPU Idle', y: 3.5 },
+    { x: 'Time To Interactive', y: 3.9 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.9 },
+    { x: 'First Meaningful Paint', y: 4.6 },
+    { x: 'First CPU Idle', y: 6.5 },
+    { x: 'Time To Interactive', y: 8.4 }
+  ],
+  [],
+  [
+    { x: 'First Contentful Paint', y: 3.9 },
+    { x: 'First Meaningful Paint', y: 3.9 },
+    { x: 'First CPU Idle', y: 5.4 },
+    { x: 'Time To Interactive', y: 6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.5 },
+    { x: 'First Meaningful Paint', y: 3.6 },
+    { x: 'First CPU Idle', y: 3.6 },
+    { x: 'Time To Interactive', y: 3.6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.5 },
+    { x: 'First Meaningful Paint', y: 3.6 },
+    { x: 'First CPU Idle', y: 3.6 },
+    { x: 'Time To Interactive', y: 3.6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.2 },
+    { x: 'First Meaningful Paint', y: 3.2 },
+    { x: 'First CPU Idle', y: 6.8 },
+    { x: 'Time To Interactive', y: 9.9 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 8.8 },
+    { x: 'First Meaningful Paint', y: 8.8 },
+    { x: 'First CPU Idle', y: 11.5 },
+    { x: 'Time To Interactive', y: 12.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 9.1 },
+    { x: 'First Meaningful Paint', y: 9.5 },
+    { x: 'First CPU Idle', y: 20.5 },
+    { x: 'Time To Interactive', y: 34.6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 4.9 },
+    { x: 'First Meaningful Paint', y: 5 },
+    { x: 'First CPU Idle', y: 7.2 },
+    { x: 'Time To Interactive', y: 9.2 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 4.9 },
+    { x: 'First Meaningful Paint', y: 5.8 },
+    { x: 'First CPU Idle', y: 12.7 },
+    { x: 'Time To Interactive', y: 14.2 }
+  ],
+  [],
+  [
+    { x: 'First Contentful Paint', y: 3 },
+    { x: 'First Meaningful Paint', y: 3 },
+    { x: 'First CPU Idle', y: 5.2 },
+    { x: 'Time To Interactive', y: 7.3 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.3 },
+    { x: 'First Meaningful Paint', y: 3.4 },
+    { x: 'First CPU Idle', y: 7.6 },
+    { x: 'Time To Interactive', y: 20.6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 4.5 },
+    { x: 'First Meaningful Paint', y: 5.4 },
+    { x: 'First CPU Idle', y: 5.4 },
+    { x: 'Time To Interactive', y: 5.4 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 4.5 },
+    { x: 'First Meaningful Paint', y: 4.8 },
+    { x: 'First CPU Idle', y: 8.8 },
+    { x: 'Time To Interactive', y: 10.1 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.2 },
+    { x: 'First Meaningful Paint', y: 2.8 },
+    { x: 'First CPU Idle', y: 10.3 },
+    { x: 'Time To Interactive', y: 11.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.6 },
+    { x: 'First Meaningful Paint', y: 2.6 },
+    { x: 'First CPU Idle', y: 2.6 },
+    { x: 'Time To Interactive', y: 2.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.3 },
+    { x: 'First Meaningful Paint', y: 3.3 },
+    { x: 'First CPU Idle', y: 3.3 },
+    { x: 'Time To Interactive', y: 20.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.2 },
+    { x: 'First Meaningful Paint', y: 3.2 },
+    { x: 'First CPU Idle', y: 4.8 },
+    { x: 'Time To Interactive', y: 5.3 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2 },
+    { x: 'First Meaningful Paint', y: 2.1 },
+    { x: 'First CPU Idle', y: 2.3 },
+    { x: 'Time To Interactive', y: 2.3 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 4.1 },
+    { x: 'First Meaningful Paint', y: 5.2 },
+    { x: 'First CPU Idle', y: 5.2 },
+    { x: 'Time To Interactive', y: 5.2 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.3 },
+    { x: 'First Meaningful Paint', y: 4.6 },
+    { x: 'First CPU Idle', y: 8.8 },
+    { x: 'Time To Interactive', y: 31.7 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.6 },
+    { x: 'First Meaningful Paint', y: 2.6 },
+    { x: 'First CPU Idle', y: 16.1 },
+    { x: 'Time To Interactive', y: 17.7 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.2 },
+    { x: 'First Meaningful Paint', y: 3.2 },
+    { x: 'First CPU Idle', y: 7 },
+    { x: 'Time To Interactive', y: 15.3 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 4.4 },
+    { x: 'First Meaningful Paint', y: 4.4 },
+    { x: 'First CPU Idle', y: 5 },
+    { x: 'Time To Interactive', y: 13.7 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.6 },
+    { x: 'First Meaningful Paint', y: 2.6 },
+    { x: 'First CPU Idle', y: 13.7 },
+    { x: 'Time To Interactive', y: 20 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.4 },
+    { x: 'First Meaningful Paint', y: 3.1 },
+    { x: 'First CPU Idle', y: 3.1 },
+    { x: 'Time To Interactive', y: 3.8 }
+  ],
+  [
     { x: 'First Contentful Paint', y: 1.7 },
     { x: 'First Meaningful Paint', y: 1.7 },
+    { x: 'First CPU Idle', y: 3.5 },
+    { x: 'Time To Interactive', y: 3.7 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 1.8 },
+    { x: 'First Meaningful Paint', y: 1.8 },
+    { x: 'First CPU Idle', y: 3.7 },
+    { x: 'Time To Interactive', y: 3.9 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 1.7 },
+    { x: 'First Meaningful Paint', y: 1.7 },
+    { x: 'First CPU Idle', y: 3.8 },
+    { x: 'Time To Interactive', y: 4 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 1.7 },
+    { x: 'First Meaningful Paint', y: 1.7 },
+    { x: 'First CPU Idle', y: 3.5 },
+    { x: 'Time To Interactive', y: 3.7 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.3 },
+    { x: 'First Meaningful Paint', y: 3.9 },
     { x: 'First CPU Idle', y: 3.9 },
-    { x: 'Time To Interactive', y: 4.3 }
+    { x: 'Time To Interactive', y: 3.9 }
   ],
   [
-    { x: 'First Contentful Paint', y: 1.8 },
-    { x: 'First Meaningful Paint', y: 1.8 },
-    { x: 'First CPU Idle', y: 1.8 },
-    { x: 'Time To Interactive', y: 1.8 }
+    { x: 'First Contentful Paint', y: 3.7 },
+    { x: 'First Meaningful Paint', y: 3.7 },
+    { x: 'First CPU Idle', y: 21.1 },
+    { x: 'Time To Interactive', y: 36.1 }
   ],
   [
-    { x: 'First Contentful Paint', y: 2.9 },
-    { x: 'First Meaningful Paint', y: 2.9 },
-    { x: 'First CPU Idle', y: 2.9 },
-    { x: 'Time To Interactive', y: 3.1 }
+    { x: 'First Contentful Paint', y: 4.3 },
+    { x: 'First Meaningful Paint', y: 4.3 },
+    { x: 'First CPU Idle', y: 8.6 },
+    { x: 'Time To Interactive', y: 12.9 }
   ],
   [
-    { x: 'First Contentful Paint', y: 2.9 },
-    { x: 'First Meaningful Paint', y: 5.5 },
-    { x: 'First CPU Idle', y: 8.4 },
-    { x: 'Time To Interactive', y: 8.9 }
+    { x: 'First Contentful Paint', y: 2.3 },
+    { x: 'First Meaningful Paint', y: 4.2 },
+    { x: 'First CPU Idle', y: 4.5 },
+    { x: 'Time To Interactive', y: 5.4 }
   ],
   [
-    { x: 'First Contentful Paint', y: 2.5 },
-    { x: 'First Meaningful Paint', y: 5.9 },
+    { x: 'First Contentful Paint', y: 2 },
+    { x: 'First Meaningful Paint', y: 2.2 },
+    { x: 'First CPU Idle', y: 4.4 },
+    { x: 'Time To Interactive', y: 7.1 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.3 },
+    { x: 'First Meaningful Paint', y: 3.3 },
+    { x: 'First CPU Idle', y: 7.6 },
+    { x: 'Time To Interactive', y: 8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3 },
+    { x: 'First Meaningful Paint', y: 3 },
+    { x: 'First CPU Idle', y: 3 },
+    { x: 'Time To Interactive', y: 3.2 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 5 },
+    { x: 'First Meaningful Paint', y: 5 },
+    { x: 'First CPU Idle', y: 12.3 },
+    { x: 'Time To Interactive', y: 13.6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.7 },
+    { x: 'First Meaningful Paint', y: 3.7 },
+    { x: 'First CPU Idle', y: 7.9 },
+    { x: 'Time To Interactive', y: 13.2 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.7 },
+    { x: 'First Meaningful Paint', y: 2.8 },
+    { x: 'First CPU Idle', y: 6.2 },
+    { x: 'Time To Interactive', y: 7.1 }
+  ],
+  [],
+  [],
+  [
+    { x: 'First Contentful Paint', y: 3.3 },
+    { x: 'First Meaningful Paint', y: 3.4 },
+    { x: 'First CPU Idle', y: 4 },
+    { x: 'Time To Interactive', y: 5.1 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3 },
+    { x: 'First Meaningful Paint', y: 3 },
+    { x: 'First CPU Idle', y: 9.1 },
+    { x: 'Time To Interactive', y: 10.4 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.1 },
+    { x: 'First Meaningful Paint', y: 3.2 },
+    { x: 'First CPU Idle', y: 7.2 },
+    { x: 'Time To Interactive', y: 9.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 5 },
+    { x: 'First Meaningful Paint', y: 5 },
+    { x: 'First CPU Idle', y: 6.2 },
+    { x: 'Time To Interactive', y: 8.1 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.6 },
+    { x: 'First Meaningful Paint', y: 2.6 },
+    { x: 'First CPU Idle', y: 10.2 },
+    { x: 'Time To Interactive', y: 24.5 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.7 },
+    { x: 'First Meaningful Paint', y: 4.1 },
     { x: 'First CPU Idle', y: 6 },
-    { x: 'Time To Interactive', y: 6.3 }
+    { x: 'Time To Interactive', y: 6.6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3 },
+    { x: 'First Meaningful Paint', y: 3 },
+    { x: 'First CPU Idle', y: 3.6 },
+    { x: 'Time To Interactive', y: 4.6 }
   ],
   [
     { x: 'First Contentful Paint', y: 1.8 },
-    { x: 'First Meaningful Paint', y: 1.8 },
-    { x: 'First CPU Idle', y: 1.8 },
+    { x: 'First Meaningful Paint', y: 4.4 },
+    { x: 'First CPU Idle', y: 8 },
+    { x: 'Time To Interactive', y: 8 }
+  ],
+  [],
+  [
+    { x: 'First Contentful Paint', y: 2.7 },
+    { x: 'First Meaningful Paint', y: 4.7 },
+    { x: 'First CPU Idle', y: 7.3 },
+    { x: 'Time To Interactive', y: 7.9 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 4.8 },
+    { x: 'First Meaningful Paint', y: 4.8 },
+    { x: 'First CPU Idle', y: 12.8 },
+    { x: 'Time To Interactive', y: 16.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.8 },
+    { x: 'First Meaningful Paint', y: 3.3 },
+    { x: 'First CPU Idle', y: 5.3 },
+    { x: 'Time To Interactive', y: 6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3 },
+    { x: 'First Meaningful Paint', y: 3.4 },
+    { x: 'First CPU Idle', y: 3.4 },
+    { x: 'Time To Interactive', y: 4.6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.3 },
+    { x: 'First Meaningful Paint', y: 3.3 },
+    { x: 'First CPU Idle', y: 8.1 },
+    { x: 'Time To Interactive', y: 9 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.6 },
+    { x: 'First Meaningful Paint', y: 2.7 },
+    { x: 'First CPU Idle', y: 2.7 },
+    { x: 'Time To Interactive', y: 2.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 0.9 },
+    { x: 'First Meaningful Paint', y: 0.9 },
+    { x: 'First CPU Idle', y: 1.3 },
     { x: 'Time To Interactive', y: 2 }
   ],
   [
-    { x: 'First Contentful Paint', y: 3.4 },
-    { x: 'First Meaningful Paint', y: 3.4 },
-    { x: 'First CPU Idle', y: 7.7 },
+    { x: 'First Contentful Paint', y: 0.8 },
+    { x: 'First Meaningful Paint', y: 0.9 },
+    { x: 'First CPU Idle', y: 1 },
+    { x: 'Time To Interactive', y: 1 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 6.8 },
+    { x: 'First Meaningful Paint', y: 6.8 },
+    { x: 'First CPU Idle', y: 8.4 },
+    { x: 'Time To Interactive', y: 9.1 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 4 },
+    { x: 'First Meaningful Paint', y: 4.4 },
+    { x: 'First CPU Idle', y: 18.6 },
+    { x: 'Time To Interactive', y: 27.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.3 },
+    { x: 'First Meaningful Paint', y: 3.3 },
+    { x: 'First CPU Idle', y: 11.4 },
+    { x: 'Time To Interactive', y: 14.7 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 4.8 },
+    { x: 'First Meaningful Paint', y: 4.8 },
+    { x: 'First CPU Idle', y: 8.9 },
+    { x: 'Time To Interactive', y: 8.9 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 1.7 },
+    { x: 'First Meaningful Paint', y: 5 },
+    { x: 'First CPU Idle', y: 6.2 },
+    { x: 'Time To Interactive', y: 6.4 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.2 },
+    { x: 'First Meaningful Paint', y: 3.7 },
+    { x: 'First CPU Idle', y: 3.9 },
+    { x: 'Time To Interactive', y: 4.5 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.7 },
+    { x: 'First Meaningful Paint', y: 2.7 },
+    { x: 'First CPU Idle', y: 9.6 },
+    { x: 'Time To Interactive', y: 15.3 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 5.8 },
+    { x: 'First Meaningful Paint', y: 6.9 },
+    { x: 'First CPU Idle', y: 7.9 },
+    { x: 'Time To Interactive', y: 8.3 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 1.9 },
+    { x: 'First Meaningful Paint', y: 1.9 },
+    { x: 'First CPU Idle', y: 1.9 },
+    { x: 'Time To Interactive', y: 1.9 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.1 },
+    { x: 'First Meaningful Paint', y: 2.6 },
+    { x: 'First CPU Idle', y: 12.7 },
+    { x: 'Time To Interactive', y: 13.6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 5.6 },
+    { x: 'First Meaningful Paint', y: 8.4 },
+    { x: 'First CPU Idle', y: 11.7 },
+    { x: 'Time To Interactive', y: 14.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 6.2 },
+    { x: 'First Meaningful Paint', y: 7.3 },
+    { x: 'First CPU Idle', y: 8.9 },
     { x: 'Time To Interactive', y: 9.2 }
   ],
   [
     { x: 'First Contentful Paint', y: 2.3 },
-    { x: 'First Meaningful Paint', y: 2.4 },
-    { x: 'First CPU Idle', y: 2.4 },
-    { x: 'Time To Interactive', y: 3.4 }
+    { x: 'First Meaningful Paint', y: 2.3 },
+    { x: 'First CPU Idle', y: 2.3 },
+    { x: 'Time To Interactive', y: 2.3 }
   ],
   [
-    { x: 'First Contentful Paint', y: 3.9 },
-    { x: 'First Meaningful Paint', y: 4.2 },
-    { x: 'First CPU Idle', y: 5.1 },
-    { x: 'Time To Interactive', y: 6.5 }
+    { x: 'First Contentful Paint', y: 1.7 },
+    { x: 'First Meaningful Paint', y: 1.7 },
+    { x: 'First CPU Idle', y: 1.7 },
+    { x: 'Time To Interactive', y: 1.7 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 6.2 },
+    { x: 'First Meaningful Paint', y: 6.2 },
+    { x: 'First CPU Idle', y: 10 },
+    { x: 'Time To Interactive', y: 11 }
+  ],
+  [],
+  [
+    { x: 'First Contentful Paint', y: 2.3 },
+    { x: 'First Meaningful Paint', y: 2.3 },
+    { x: 'First CPU Idle', y: 6 },
+    { x: 'Time To Interactive', y: 7.8 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.4 },
+    { x: 'First Meaningful Paint', y: 2.5 },
+    { x: 'First CPU Idle', y: 2.8 },
+    { x: 'Time To Interactive', y: 3 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3 },
+    { x: 'First Meaningful Paint', y: 3 },
+    { x: 'First CPU Idle', y: 6.6 },
+    { x: 'Time To Interactive', y: 8.6 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.1 },
+    { x: 'First Meaningful Paint', y: 5.5 },
+    { x: 'First CPU Idle', y: 10.1 },
+    { x: 'Time To Interactive', y: 23.2 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 6.7 },
+    { x: 'First Meaningful Paint', y: 9.1 },
+    { x: 'First CPU Idle', y: 11.3 },
+    { x: 'Time To Interactive', y: 12.5 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 2.2 },
+    { x: 'First Meaningful Paint', y: 2.2 },
+    { x: 'First CPU Idle', y: 5 },
+    { x: 'Time To Interactive', y: 6.1 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 1.1 },
+    { x: 'First Meaningful Paint', y: 1.2 },
+    { x: 'First CPU Idle', y: 3.8 },
+    { x: 'Time To Interactive', y: 4.5 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.7 },
+    { x: 'First Meaningful Paint', y: 3.7 },
+    { x: 'First CPU Idle', y: 9 },
+    { x: 'Time To Interactive', y: 9.4 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.4 },
+    { x: 'First Meaningful Paint', y: 3.8 },
+    { x: 'First CPU Idle', y: 4 },
+    { x: 'Time To Interactive', y: 5.4 }
+  ],
+  [
+    { x: 'First Contentful Paint', y: 3.4 },
+    { x: 'First Meaningful Paint', y: 3.9 },
+    { x: 'First CPU Idle', y: 4.1 },
+    { x: 'Time To Interactive', y: 5.5 }
   ]
 ]
